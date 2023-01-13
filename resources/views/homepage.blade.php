5fc0e3765e83262ab8a1cb8d62dddc5c188eaa6f
SimoVisc <simonaviscardivs@gmail.com>
2023-01-13 18:02:50 +0100

    Aggiunge header

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('page-title')
+    Homepage
+@endsection
+
+@section('content')
+    <section>
+        <h2>CURRENT SERIES</h2>
+    </section>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,28 @@
+<header>
+    <div class="header-top bg-primary text-light">
+        <div class="container d-flex justify-content-end">
+            <small>DC POWER""VISA@</small>
+            <small>ADDITIONAL DC SITES <i class="fa-solid fa-caret-down"></i></small>
+        </div>
+    </div>
+    <div class="container d-flex align-items-center justify-content-between">
+        <div class="container-logo py-4 ">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo">
+        </div>
+        <nav class=" d-flex justify-content-end ">
+            <ul class="list-unstyled d-flex">
+                <li><a class="menu-item" href=""> CHARACTERS</a></li>
+                <li><a class="menu-item menu-item--active" href="">COMICS</a></li>
+                <li><a class="menu-item" href="">MOVIES</a></li>
+                <li><a class="menu-item" href="">TV</a></li>
+                <li><a class="menu-item" href="">GAMES</a></li>
+                <li><a class="menu-item" href="">COLLECTIBLES</a></li>
+                <li><a class="menu-item" href="">VIDEO</a></li>
+                <li><a class="menu-item" href="">FANS</a></li>
+                <li><a class="menu-item" href="">NEWS</a></li>
+                <li><a class="menu-item" href="">SHOP <i class="fa-solid fa-caret-down text-primary "></i></a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
