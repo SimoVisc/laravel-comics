3e5eda9b5ec24bd86939b06d6bfffb30ec6df306
SimoVisc <simonaviscardivs@gmail.com>
2023-01-14 14:49:11 +0100

    Inserise il database all'interno della rotta comics

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('comics');
+    $data = [
+        "comics" => config("db")
+    ];
+
+    return view('comics', $data);
 }) -> name('comics');
 
 Route::get('/characters', function () {
