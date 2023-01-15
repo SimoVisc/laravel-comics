c337a5e145b1f93d2331f373259aeff3d8aa0b43
SimoVisc <simonaviscardivs@gmail.com>
2023-01-15 18:28:54 +0100

    termina comics e crea un nuovo foglio css che lo contiene

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -18,6 +18,39 @@
                     </div>
                 @endforeach
             </div>
+            <div class="btn-container d-flex justify-content-center">
+                <button type="button" class="comics-btn">Load More</button>
+            </div>
+        </div>
+        <div class="comics-bottom">
+            <div class="container">
+                <ul class="d-flex align-items-center">
+                    <li>
+                        <img src="{{ Vite::asset('resources\img\buy-comics-digital-comics.png') }}" alt="digital-comics">
+                        <div class="comics-text-container">
+                            <span>DIGITAL COMICS</span>
+                        </div>
+                    </li>
+                    <li>
+                        <img src="{{ Vite::asset('resources\img\buy-comics-merchandise.png') }}" alt="digital-comics">
+                        <span>DC MERCHANDISE</span>
+                    </li>
+                    <li>
+                        <img src="{{ Vite::asset('resources\img\buy-comics-subscriptions.png') }}"
+                            alt="comics-subscriptions">
+                        <span>SUBSCRIPTION</span>
+                    </li>
+                    <li>
+                        <img class="img-big" src="{{ Vite::asset('resources\img\buy-comics-shop-locator.png') }}"
+                            alt="shop-locator">
+                        <span>COMIC SHOP LOCATOR</span>
+                    </li>
+                    <li>
+                        <img src="{{ Vite::asset('resources\img\buy-dc-power-visa.svg') }}" alt="power-visa">
+                        <span>DC POWER VISA</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     </section>
 @endsection
